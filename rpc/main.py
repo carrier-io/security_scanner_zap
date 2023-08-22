5d2c891078861c80d84f56eec5bb5653a387b53b
Ivan Krakhmaliuk <ivan_krakhmaliuk@epam.com>
2023-08-22 23:38:56 +0300

    Log data for debug

--- rpc/main.py
+++ rpc/main.py
@@ -11,7 +11,7 @@ class RPC:
     @rpc_tools.wrap_exceptions(RuntimeError)
     def make_dusty_config(self, context, test_params, scanner_params):
         """ Prepare dusty config for this scanner """
-        # log.info("Test params: %s", test_params)
+        log.info("Test params: %s", test_params)
         log.info("Scanner params: %s", scanner_params)
         result = {
             "target": test_params["urls_to_scan"][0],
